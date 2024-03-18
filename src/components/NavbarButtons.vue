08b97dacbe61f5f00182afd37df753c9196f6c44
zhcz <348316166@qq.com>
2024-03-18 17:47:04 +0800

    some

--- src/components/NavbarButtons.vue
+++ src/components/NavbarButtons.vue
@@ -7,7 +7,7 @@ const { isDarkTheme } = toRefs(styleStore);
 </script>
 
 <template>
-  <c-tooltip :tooltip="$t('home.nav.github')" position="bottom">
+  <!-- <c-tooltip :tooltip="$t('home.nav.github')" position="bottom">
     <c-button
       circle
       variant="text"
@@ -37,7 +37,8 @@ const { isDarkTheme } = toRefs(styleStore);
     <c-button circle variant="text" to="/about" :aria-label="$t('home.nav.aboutLabel')">
       <n-icon size="25" :component="InfoCircle" />
     </c-button>
-  </c-tooltip>
+  </c-tooltip> -->
+
   <c-tooltip :tooltip="isDarkTheme ? $t('home.nav.lightMode') : $t('home.nav.darkMode')" position="bottom">
     <c-button circle variant="text" :aria-label="$t('home.nav.mode')" @click="() => styleStore.toggleDark()">
       <n-icon v-if="isDarkTheme" size="25" :component="Sun" />
